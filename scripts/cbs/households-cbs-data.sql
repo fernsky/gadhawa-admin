6016b2dd6bffb82d3f7f850124ab6dafbbb3b315
Julius Ceaser <trilochanbhusal000@gmail.com>
2025-05-15 15:26:44 +0545

    feat: make cbs-comparison feature

--- scripts/cbs/households-cbs-data.sql
+++ scripts/cbs/households-cbs-data.sql
@@ -13,13 +13,12 @@ CREATE TABLE IF NOT EXISTS household_cbs (
 -- Insert ward-wise household data
 INSERT INTO household_cbs (id, ward_no, total_households, created_at) 
 VALUES 
-  ('house_ward_1', 1, 476, NOW()),
-  ('house_ward_2', 2, 562, NOW()),
-  ('house_ward_3', 3, 812, NOW()),
-  ('house_ward_4', 4, 415, NOW()),
-  ('house_ward_5', 5, 944, NOW()),
-  ('house_ward_6', 6, 982, NOW()),
-  ('house_ward_7', 7, 1034, NOW()),
-  ('house_ward_8', 8, 1041, NOW()),
-  ('house_ward_9', 9, 1401, NOW()),
-  ('house_ward_10', 10, 906, NOW());
+  ('house_ward_1', 1, 1688, NOW()),
+  ('house_ward_2', 2, 1169, NOW()),
+  ('house_ward_3', 3, 1135, NOW()),
+  ('house_ward_4', 4, 1265, NOW()),
+  ('house_ward_5', 5, 1482, NOW()),
+  ('house_ward_6', 6, 1714, NOW()),
+  ('house_ward_7', 7, 1320, NOW()),
+  ('house_ward_8', 8, 191, NOW());
+
--- scripts/cbs/population-cbs-data.sql
+++ scripts/cbs/population-cbs-data.sql
@@ -13,13 +13,12 @@ CREATE TABLE IF NOT EXISTS population_gender_wise_cbs (
 -- Insert ward-wise population data
 INSERT INTO population_gender_wise_cbs (id, ward_no, total_population, total_male, total_female)
 VALUES 
-  ('pop_ward_1', 1, 1994, 973, 1021),
-  ('pop_ward_2', 2, 2192, 1063, 1129),
-  ('pop_ward_3', 3, 3390, 1647, 1743),
-  ('pop_ward_4', 4, 1682, 838, 844),
-  ('pop_ward_5', 5, 3892, 1839, 2053),
-  ('pop_ward_6', 6, 3991, 1924, 2067),
-  ('pop_ward_7', 7, 4169, 1967, 2202),
-  ('pop_ward_8', 8, 4088, 1925, 2163),
-  ('pop_ward_9', 9, 5564, 2576, 2988),
-  ('pop_ward_10', 10, 3542, 1663, 1879);
+  ('pop_ward_1', 1, 7561, 3619, 3942),
+  ('pop_ward_2', 2, 5596, 2754, 2842),
+  ('pop_ward_3', 3, 5049, 2439, 2610),
+  ('pop_ward_4', 4, 5983, 2971, 3012),
+  ('pop_ward_5', 5, 6901, 3374, 3527),
+  ('pop_ward_6', 6, 7847, 3819, 4028),
+  ('pop_ward_7', 7, 6111, 2925, 3186),
+  ('pop_ward_8', 8, 850, 407, 443);
+
